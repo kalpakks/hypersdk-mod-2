34661cdf5c88d2307308ffdcf731619681c284c9
kalpakks <161148603+kalpakks@users.noreply.github.com>
2024-03-14 21:40:50 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "metacrafters"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "metacrafters"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MT"
 )
 
 var ID ids.ID
